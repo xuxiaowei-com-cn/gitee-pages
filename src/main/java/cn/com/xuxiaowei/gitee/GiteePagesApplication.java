6bd6d8affe7b3c4152be07a1da90d184cdc6e166
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-04-01 08:21:38 +0800

    :tada: init

---
+++ src/main/java/cn/com/xuxiaowei/gitee/GiteePagesApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.gitee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiteePagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiteePagesApplication.class, args);
+	}
+
+}
